1f12e6f9137376440399477b539e229d2ee868e2
EC2 Default User <ec2-user@ip-172-31-8-23.ap-northeast-1.compute.internal>
2023-02-10 08:42:17 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,33 @@
 class BooksController < ApplicationController
   def new
+    @books=Book.new
   end
-
+  
+  def create
+    #データを受け取り新規登録するためのインスタンスインスタンス作成
+    book = Book.new(book_params)
+    #データをデータベースへ保存する
+    book.save
+    #book一覧画面へリダイレクト
+    redirect_to book_path(book.id)
+  end
+  
   def index
+   @book = Book.new   
    @books = Book.all
   end
 
   def show
+   @book =Book.find(paramd[:id])
   end
 
   def edit
   end
+
+ private
+ #ストロングパラメーター
+  def list_params
+     params.require(:book).permit(:title,:body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books/edit'
   root 'homes#top'
-  
+  post 'books'=>'books#cerate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#どの投稿データを表示するか判別するための記述
+get 'lists/:id' => 'lists#show'
 end
