debfe3e2e0a45e3190c80fd0cc93ffa7cf88ac90
EC2 Default User <ec2-user@ip-172-31-8-23.ap-northeast-1.compute.internal>
2023-02-16 13:28:12 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @books=Book.new
   end
-  
+
   def create
     #データを受け取り新規登録するためのインスタンスインスタンス作成
     @book = Book.new(book_params)
@@ -12,12 +12,13 @@ class BooksController < ApplicationController
     flash[:notice] = "successfully create."
     redirect_to book_path(@book.id)
     else
+     @books = Book.all
      render :index
     end
   end
-  
+
   def index
-   @book = Book.new   
+   @book = Book.new
    @books = Book.all
   end
 
@@ -30,19 +31,22 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "successfully updated."
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
   end
-  
+
   def destroy
    book= Book.find(params[:id])
    book.destroy
    flash[:notice] = "successfully destroy."
-   redirect_to '/books/'
+   redirect_to '/books'
   end
-  
+
  private
  #ストロングパラメーター
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  get '/' => 'home#top'
+  root to: 'home#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #どの投稿データを表示するか判別するための記述
